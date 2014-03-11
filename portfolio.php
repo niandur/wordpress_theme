eaf31261fbac2c16b8b0d47f175830ef48944d58
niandur <evallovetc@gmail.com>
2014-03-11 15:46:19 +0100

    probes

--- portfolio.php
+++ portfolio.php
@@ -3,4 +3,21 @@
  Template Name Archives
 */
  get_header(); ?>
- */
+<div id="container" role="main">
+	<?php the_post(); ?>
+	<h1 class="entry-title"><?php the_title(); ?></h1>
+	<?php get_search_form(); ?>
+	<h2> Archives By Month:</h2>
+	<ul><?php wp_get_archives('typemonthly'); ?>
+		<h2>
+			Archives by Subject:
+		</h2>
+		<ul>
+			<?php wp_list_categories(); ?>
+		</ul>
+	</ul>
+</div> 
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
+
+*/
